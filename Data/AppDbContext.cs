82586e30b141a18a5c18a0dadd6963a09b3c4fef
jolly <jolly@JOLLYPATEL>
2025-10-08 18:47:45 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatingApp.Entities;
+
+namespace DatingApp.Data
+{
+    public class AppDbContext : DbContext{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
