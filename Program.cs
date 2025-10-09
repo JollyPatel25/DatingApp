f82afdca17f184cdebb3e784d8cbeea8b36387e4
Jolly PAtel <jollypatel2504@gmail.com>
2025-10-09 23:23:14 +0530

    End of section 3 on 9th october

--- Program.cs
+++ Program.cs
@@ -4,24 +4,42 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Configure DbContext with SQLite
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// Configure OpenAPI (Swagger)
 builder.Services.AddOpenApi();
+
+// Configure CORS with named policy
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200") // Angular frontend HTTPS
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); // Swagger UI in development
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+// Apply CORS BEFORE Authorization
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
